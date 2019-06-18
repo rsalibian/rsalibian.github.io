2751ecfba2f66a7b26e6d32ce19af61825b71bb1
Raffi Salibian <rsalibian@aol.com>
2019-06-17 18:05:17 -0700

    next update

--- sketch.js
+++ sketch.js
@@ -69,72 +69,17 @@ function classify() {
 
   const features = featureExtractor.infer(video);
 
-  // Use knnClassifier to classify which label do these features belong to
-  // You can pass in a callback function `gotResults` to knnClassifier.classify function
   knnClassifier.classify(features, gotResults);
-  // You can also pass in an optional K value, K default to 3
-  // knnClassifier.classify(features, 3, gotResults);
 
-  // You can also use the following async/await function to call knnClassifier.classify
-  // Remember to add `async` before `function predictClass()`
-  // const res = await knnClassifier.classify(features);
-  // gotResults(null, res);
 }
 
 
 function createButtons() {
-  // When the A button is pressed, add the current frame
-  // from the video with a label of "rock" to the classifier
-  buttonA = select('#addClassRock');
-  buttonA.mousePressed(function() {
-    addExample('Rock');
-  });
-
-  // When the B button is pressed, add the current frame
-  // from the video with a label of "paper" to the classifier
-  buttonB = select('#addClassPaper');
-  buttonB.mousePressed(function() {
-    addExample('Paper');
-  });
-
-  // When the C button is pressed, add the current frame
-  // from the video with a label of "scissor" to the classifier
-  buttonC = select('#addClassScissor');
-  buttonC.mousePressed(function() {
-    addExample('Scissor');
-  });
-
-  // Reset buttons
-  resetBtnA = select('#resetRock');
-  resetBtnA.mousePressed(function() {
-    clearLabel('Rock');
-  });
-	
-  resetBtnB = select('#resetPaper');
-  resetBtnB.mousePressed(function() {
-    clearLabel('Paper');
-  });
-	
-  resetBtnC = select('#resetScissor');
-  resetBtnC.mousePressed(function() {
-    clearLabel('Scissor');
-  });
-
+  
   // Predict button
   buttonPredict = select('#buttonPredict');
   buttonPredict.mousePressed(classify);
 
-  // Clear all classes button
-  buttonClearAll = select('#clearAll');
-  buttonClearAll.mousePressed(clearAllLabels);
-
-  // Load saved classifier dataset
-  buttonSetData = select('#load');
-  buttonSetData.mousePressed(loadMyKNN);
-
-  // Get classifier dataset
-  buttonGetData = select('#save');
-  buttonGetData.mousePressed(saveMyKNN);
 }
 
 // Show the results
@@ -167,23 +112,6 @@ function updateCounts() {
   select('#exampleRock').html(counts['Rock'] || 0);
   select('#examplePaper').html(counts['Paper'] || 0);
   select('#exampleScissor').html(counts['Scissor'] || 0);
-}
-
-// Clear the examples in one label
-function clearLabel(label) {
-  knnClassifier.clearLabel(label);
-  updateCounts();
-}
-
-// Clear all the examples in all labels
-function clearAllLabels() {
-  knnClassifier.clearAllLabels();
-  updateCounts();
-}
-
-// Save dataset as myKNNDataset.json
-function saveMyKNN() {
-  knnClassifier.save('myKNNDataset');
 }
 
 // Load dataset to the classifier
