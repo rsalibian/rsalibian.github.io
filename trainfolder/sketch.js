908d6dc65678ac683c861de2ebaabffa710c2304
rsalibian <51869006+rsalibian@users.noreply.github.com>
2019-07-08 17:41:49 -0700

    Add files via upload

--- trainfolder/sketch.js
+++ trainfolder/sketch.js
@@ -43,23 +43,18 @@ function addExample(label) {
 
 //add from file
 function addExamplefromfile() {
-        
-       
-  call addexamplenow("image1");
-  
-  
-}
+    
+       for (i = 1; i < 5; i++) {
 
-function addexamplenow(imageIdelement) {
+         console.log(document.getElementById("image" + i))
     
-       console.log(document.getElementById(imageIdelement))
-    
-       const image = document.getElementById(imageIdelement);
+         const image = document.getElementById("image" + i);
    
-       const features = featureExtractor.infer(image);
+         const features = featureExtractor.infer(image);
 
-       knnClassifier.addExample(features, 'stress fracture');
-    
+         knnClassifier.addExample(features, 'stress fracture');
+       
+       }
     
        //console.log(document.getElementById("image1"))
     
@@ -68,8 +63,11 @@ function addexamplenow(imageIdelement) {
        //const features = featureExtractor.infer(image);
 
        //knnClassifier.addExample(features, 'stress fracture');
-    
+  
+  
 }
+
+
     
 
 // Predict the current frame.
