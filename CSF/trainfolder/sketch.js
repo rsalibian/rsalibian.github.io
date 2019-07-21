c550fc6059f17fcedb667ad9106bdafa26e607c3
Raffi Salibian <rsalibian@aol.com>
2019-07-20 20:11:52 -0700

    Update sketch.js

--- CSF/trainfolder/sketch.js
+++ CSF/trainfolder/sketch.js
@@ -18,7 +18,9 @@ function setup() {
 //add from file
 function addExamplefromfile() {
     
-     var sourceFolder = new sourceFolder ("https://rsalibian.github.io/CSF/trainfolder/images")
+     var sourceFolder1 = new sourceFolder;
+    
+     sourceFolder1.url = "https://rsalibian.github.io/CSF/trainfolder/images/";
     
      var fileList = sourceFolder.getFiles();  
     
